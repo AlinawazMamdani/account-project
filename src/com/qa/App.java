bbf9db83f32e3a35d9681c9f44dfa90d72885c9a
alinawaz <alinawazmamdani@gmail.com>
2019-04-16 14:43:34 +0100

    Changed static methods from service

--- src/com/qa/App.java
+++ src/com/qa/App.java
@@ -1,39 +1,41 @@
 package com.qa;
+
 import java.util.Scanner;
 import java.util.HashMap;
 import java.util.Map;
 
 public class App {
 
-public static void main (String[] args ) {
-	Scanner sc = new Scanner(System.in);
-	boolean finished= false;
-	while (finished==false) {
-		System.out.println("press '1' for add account Press '2' for get account Press '3' to get info as a json press 'f' to finish press '4' to get all map as json" );
-		switch (sc.nextLine()){
-		case "1":
-			Service.insertDetails();
-			continue;
-		case "2":
-			System.out.println("enter the key value for the account");
-			Service.getDetails(sc.nextLine());
-			continue;
-		case "3":
-		System.out.println("enter the key value for the account");
-		Service.getJson(sc.nextLine());
-		continue;
-		case "4":
-			Service.getAllJson();
-		case "5":
-			String json = Service.getAllJson();
-			Service.jsonToMap(json);
-		continue;
-		case "f":
-			System.out.println("thanks for using this program");
-		    finished = true;
-		}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Service serv = new Service();
+		boolean finished = false;
+		while (finished == false) {
+			System.out.println(
+					"press '1' for add account Press '2' for get account Press '3' to get info as a json press 'f' to finish press '4' to get all map as json");
+			switch (sc.nextLine()) {
+			case "1":
+				serv.insertDetails();
+				continue;
+			case "2":
+				System.out.println("enter the key value for the account");
+				serv.getDetails(sc.nextLine());
+				continue;
+			case "3":
+				System.out.println("enter the key value for the account");
+				serv.getJson(sc.nextLine());
+				continue;
+			case "4":
+				serv.getAllJson();
+			case "5":
+				String json = serv.getAllJson();
+				serv.jsonToMap(json);
+				continue;
+			case "f":
+				System.out.println("thanks for using this program");
+				finished = true;
+			}
 		}
 	}
-
 
 }
--- src/com/qa/Service.java
+++ src/com/qa/Service.java
@@ -5,53 +5,62 @@ import java.util.Map;
 import java.util.Scanner;
 
 import com.google.gson.Gson;
+
 public class Service {
-public static Map <String, Account> account = new HashMap<String, Account>();
-public static void insertDetails() {
-	Scanner sc = new Scanner(System.in);
-	String firstName=sc.nextLine();
-	String lastName=sc.nextLine();
-	Account accounty=new Account(firstName, lastName);
-	account.put(sc.nextLine(), accounty);
-}
-public static void retreiveAccount(Account accounty) {
-	System.out.println(accounty.getFirstName());
-	System.out.println(accounty.getLastName());
-	System.out.println(accounty.getAccountNumber());
-}
-public static void getDetails(String kv) {
-	Account accounty=account.get(kv);
-	System.out.println(accounty.getFirstName());
-	System.out.println(accounty.getLastName());
-	System.out.println(accounty.getAccountNumber());
-}
-public static void getJson(String kv) {
-	Account accounty=account.get(kv);
-	Gson gson = new Gson(); 
-	String json=gson.toJson(accounty);
-	System.out.println(json);
-}
-public static String getAllJson() {
-	Gson gson = new Gson(); 
-	String json=gson.toJson(account);
-	System.out.println(json);
-	return json;
-}
-public static void jsonToMap(String json) {
-   Map<String, Account> map= new HashMap<String, Account>();
-   Gson gson = new Gson();
-   map =(Map<String,Account>) gson.fromJson(json,map.getClass());
-   System.out.println(gson.toJson(map));
-}
-public static int countName(String name) {
-    //String json=getAllJson();
-    int nameCount=0;
-    for (Account a:account.values()) {	
-    	if (a.getFirstName()==name) {
-    		nameCount+=1;	
-    	}
-    }
-	return nameCount;
-}
+	public Map<String, Account> account = new HashMap<String, Account>();
+
+	public void insertDetails() {
+		Scanner sc = new Scanner(System.in);
+		String firstName = sc.nextLine();
+		String lastName = sc.nextLine();
+		Account accounty = new Account(firstName, lastName);
+		account.put(sc.nextLine(), accounty);
+	}
+
+	public void retreiveAccount(Account accounty) {
+		System.out.println(accounty.getFirstName());
+		System.out.println(accounty.getLastName());
+		System.out.println(accounty.getAccountNumber());
+	}
+
+	public void getDetails(String kv) {
+		Account accounty = account.get(kv);
+		System.out.println(accounty.getFirstName());
+		System.out.println(accounty.getLastName());
+		System.out.println(accounty.getAccountNumber());
+	}
+
+	public void getJson(String kv) {
+		Account accounty = account.get(kv);
+		Gson gson = new Gson();
+		String json = gson.toJson(accounty);
+		System.out.println(json);
+	}
+
+	public String getAllJson() {
+		Gson gson = new Gson();
+		String json = gson.toJson(account);
+		System.out.println(json);
+		return json;
+	}
+
+	public void jsonToMap(String json) {
+		Map<String, Account> map = new HashMap<String, Account>();
+		Gson gson = new Gson();
+		map = (Map<String, Account>) gson.fromJson(json, map.getClass());
+		System.out.println(gson.toJson(map));
+	}
+
+	public int countName(String name) {
+		// String json=getAllJson();
+		int nameCount = 0;
+		for (Account a : account.values()) {
+			System.out.println(a.getFirstName());
+			if (a.getFirstName() == name) {
+				nameCount += 1;
+			}
+		}
+		return nameCount;
+	}
 
 }
